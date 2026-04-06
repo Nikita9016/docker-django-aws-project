8444971e41106cb5e017e0f0193c8132eb7aa7a6
anamika-singh-dev <anamika.singh@vonage.com>
2026-04-06 16:18:15 +0530

    update changes

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello Docker django ")
+    return HttpResponse("Hello django ")
