8fcf5770cf4205abe97eceeeb6f5dec48608ddda
anamika-singh-dev <anamika.singh@vonage.com>
2026-04-06 12:27:38 +0530

    update code

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Django Docker AWS Deployment Working")
+    return HttpResponse("Hello Docker")
